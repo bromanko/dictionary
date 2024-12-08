1a9a971a46e99ec86abde69c9665b7f785fa35ef
Brian Romanko <hello@bromanko.com>
2024-12-07 17:31:16 -0800

    Adjust database schema

--- db/migrations/001-create-words.sql
+++ db/migrations/001-create-words.sql
@@ -1,6 +1,5 @@
 CREATE TABLE words (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     word TEXT NOT NULL,
-    language TEXT NOT NULL,
-    part_of_speech TEXT NOT NULL
+    language TEXT NOT NULL
 )
--- db/migrations/004-create-synoyms.sql
+++ db/migrations/004-create-synoyms.sql
@@ -1,6 +1,6 @@
 CREATE TABLE synonyms (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     word_id INTEGER NOT NULL,
-    synonyms TEXT NOT NULL,
+    synonym TEXT NOT NULL,
     FOREIGN KEY (word_id) REFERENCES words(id)
 )
